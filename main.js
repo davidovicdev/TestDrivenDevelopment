599a9923644864df708308861d2787f458f8e3dd
Matija Davidovic <davidovicdev@gmail.com>
2022-10-12 17:02:00 +0200

    next one

--- main.js
+++ main.js
@@ -1,76 +1,35 @@
-/* 1. [Exercise] Using TDD implement other functions for:
-Subtracting two numbers
-Multiplication of two numbers
-Dividing two numbers
-Addition a few numbers in one call (call addCalculator with more than 2 arguments and get correct result) */
-
-/* 2. [Exercise] Write new tests that use already implemented functions, but also handle edge cases and possible issues:
-Add handling error that will occur when function is called with only 1 parameter, or without any parameters (all currently existing functions)
-Add handling error that will occur when function for dividing is called with second parameter 0 (when you try to divide by 0)
-Add handling error that will occur when one of multiplication parameters is string */
-const MESSAGE_ONLY_NUMBERS = "You can pass only numbers";
-const MESSAGE_AT_LEAST_MY_NUMBER = "You need to pass at least one number";
-const MESSAGE_CANT_BE_ZERO = "You can't divide by zero";
-const MESSAGE_ARRAY_OF_NUMBERS = "You need to pass a array of numbers";
-function isNumber(a) {
-  return typeof a == "number" ? true : false;
+function validation(numberOfArguments, number1, number2) {
+  if (numberOfArguments != 2) {
+    throw new Error(`You need to pass ${numberOfArguments} numbers exactly`);
+  }
+  if (typeof number1 != "number" || typeof number2 != "number") {
+    throw new Error("You need to pass numbers");
+  }
 }
-function substract(a, b) {
-  return isNumber(a) && isNumber(b) ? a - b : MESSAGE_ONLY_NUMBERS;
+function substractTwoNumbers(a, b) {
+  validation(arguments.length, a, b);
+  if (a < b) {
+    throw new Error("First number needs to be bigger then the second one");
+  } else {
+    return a - b;
+  }
 }
-function multiple(a, b) {
-  return isNumber(a) && isNumber(b) ? a * b : MESSAGE_ONLY_NUMBERS;
+function multiplyTwoNumbers(a, b) {
+  validation(arguments.length, a, b);
+  return a * b;
 }
-function dividingTwoNumbers(a, b) {
+function divideTwoNumbers(a, b) {
+  validation(arguments.length, a, b);
   if (b == 0) {
-    return MESSAGE_CANT_BE_ZERO;
-  }
-  return isNumber(a) && isNumber(b) ? a / b : MESSAGE_ONLY_NUMBERS;
-}
-function addNumbers() {
-  var total = 0;
-  if (arguments.length > 0) {
-    for (let i = 0; i < arguments.length; i++) {
-      if (typeof arguments[i] != "number") {
-        return MESSAGE_ONLY_NUMBERS;
-      }
-      total += arguments[i];
-    }
+    throw new Error("You can't divide by zero");
   } else {
-    return MESSAGE_AT_LEAST_MY_NUMBER;
+    return a / b;
   }
-  return total;
 }
-/* 4. [Exercise] Using TDD implement function (you can use more private functions to separate logic) for analyze an array of numbers.
-Function takes an array of numbers, analyze it, and return object that has information about number of elements, average value of elements, maximum and minimum value. Implement handling error that function throw if one of elements is string, or undefined. */
-function arrayFunctions(arrayOfNumbers) {
-  if (
-    arguments.length != 1 ||
-    typeof arrayOfNumbers != "object" ||
-    arrayOfNumbers.some((x) => typeof x != "number")
-  ) {
-    return MESSAGE_ARRAY_OF_NUMBERS;
-  } else {
-    var object = {};
-    var averageValue = 0;
-    arrayOfNumbers.forEach((number) => {
-      averageValue += number;
-    });
-    object.countNumbers = arrayOfNumbers.length;
-    object.averageValue = averageValue / arrayOfNumbers.length;
-    object.minimumValue = Math.min(...arrayOfNumbers);
-    object.maximumValue = Math.max(...arrayOfNumbers);
-  }
-  return object;
-}
+function addNumbers() {}
 module.exports = {
-  substract,
-  multiple,
-  dividingTwoNumbers,
+  substractTwoNumbers,
+  multiplyTwoNumbers,
+  divideTwoNumbers,
   addNumbers,
-  arrayFunctions,
-  MESSAGE_AT_LEAST_MY_NUMBER,
-  MESSAGE_ONLY_NUMBERS,
-  MESSAGE_CANT_BE_ZERO,
-  MESSAGE_ARRAY_OF_NUMBERS,
 };
--- main.spec.js
+++ main.spec.js
@@ -1,50 +1,29 @@
-/* [Exercise] Using TDD implement other functions for:
-Subtracting two numbers
-Multiplication of two numbers
-Dividing two numbers
-Addition a few numbers in one call (call addCalculator with more than 2 arguments and get correct result) */
+/* EXERCICE 2 */
 const myFunctions = require("./main");
-test("Substract two numbers", () => {
-  expect(myFunctions.substract(6, null)).toBe(myFunctions.MESSAGE_ONLY_NUMBERS);
+test("Substract 10 and 9", () => {
+  expect(myFunctions.substractTwoNumbers(10, 9)).toBe(1);
 });
-test("Multiplication of two numbers", () => {
-  expect(myFunctions.multiple(2, 3)).toBe(6);
+test("Substract 10, 9 and null", () => {
+  expect(() => {
+    myFunctions.substractTwoNumbers(10, 9, null);
+  }).toThrow(Error);
 });
-test("Dividng two numbers", () => {
-  expect(myFunctions.dividingTwoNumbers(6, 2)).toBe(3);
+test("Multiply 2 and 22", () => {
+  expect(myFunctions.multiplyTwoNumbers(2, 22)).toBe(44);
 });
-test("Add more numbers", () => {
-  expect(myFunctions.addNumbers(2, 3, "pera")).toBe(
-    myFunctions.MESSAGE_ONLY_NUMBERS
-  );
+test("Multiply only null", () => {
+  expect(() => {
+    myFunctions.multiplyTwoNumbers(null);
+  }).toThrow(Error);
 });
-test("Add more numbers", () => {
-  expect(myFunctions.addNumbers(14, 2, null, undefined, "zika")).toBe(
-    myFunctions.MESSAGE_ONLY_NUMBERS
-  );
+test("Divide 10 and 5", () => {
+  expect(myFunctions.divideTwoNumbers(10, 5)).toBe(2);
 });
-/* 3. [Exercise] Write new tests that use already implemented functions, but also handle edge cases and possible issues:
-Add handling error that will occur when function is called with only 1 parameter, or without any parameters (all currently existing functions)
-Add handling error that will occur when function for dividing is called with second parameter 0 (when you try to divide by 0)
-Add handling error that will occur when one of multiplication parameters is string */
-test("Add more numbers", () => {
-  expect(myFunctions.addNumbers()).toBe(myFunctions.MESSAGE_AT_LEAST_MY_NUMBER);
+test("Divide 10 and 0", () => {
+  expect(() => {
+    myFunctions.divideTwoNumbers(10, 0);
+  }).toThrow(Error);
 });
-test("Dividng two numbers", () => {
-  expect(myFunctions.dividingTwoNumbers(2, 0)).toBe(
-    myFunctions.MESSAGE_CANT_BE_ZERO
-  );
-});
-test("Multiplication of two numbers", () => {
-  expect(myFunctions.multiple(2, "PERA")).toBe(
-    myFunctions.MESSAGE_ONLY_NUMBERS
-  );
-});
-/* 4. [Exercise] Using TDD implement function (you can use more private functions to separate logic) for analyze an array of numbers.
-Function takes an array of numbers, analyze it, and return object that has information about number of elements, average value of elements, maximum and minimum value. Implement handling error that function throw if one of elements is string, or undefined. */
-var analyzeObjectStatistic = myFunctions.arrayFunctions([1, 2, null, 4, 5]);
-test("Jokes with array of numbers", () => {
-  expect(analyzeObjectStatistic.averageValue).toBe(3);
-  expect(analyzeObjectStatistic.minimumValue).toBe(1);
-  expect(analyzeObjectStatistic.maximumValue).toBe(5);
+test("Add 1, 4, 10 and 22", () => {
+  expect(myFunctions.addNumbers([1, 4, 10, 22])).toBe(37);
 });
