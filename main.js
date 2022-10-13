5bd53fd780eb29579c1b55f89832cbb86c7e95ac
Matija Davidovic <davidovicdev@gmail.com>
2022-10-13 10:16:57 +0200

    done exercise 4

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function validation(numberOfArguments, number1, number2) {
+function validationForTwoParameters(numberOfArguments, number1, number2) {
   if (numberOfArguments != 2) {
-    throw new Error(`You need to pass ${numberOfArguments} numbers exactly`);
+    throw new Error(`You need to pass 2 numbers exactly`);
   }
   if (typeof number1 != "number" || typeof number2 != "number") {
     throw new Error("You need to pass numbers");
   }
 }
 function substractTwoNumbers(a, b) {
-  validation(arguments.length, a, b);
+  validationForTwoParameters(arguments.length, a, b);
   if (a < b) {
     throw new Error("First number needs to be bigger then the second one");
   } else {
@@ -15,21 +15,51 @@ function substractTwoNumbers(a, b) {
   }
 }
 function multiplyTwoNumbers(a, b) {
-  validation(arguments.length, a, b);
+  validationForTwoParameters(arguments.length, a, b);
   return a * b;
 }
 function divideTwoNumbers(a, b) {
-  validation(arguments.length, a, b);
+  validationForTwoParameters(arguments.length, a, b);
   if (b == 0) {
     throw new Error("You can't divide by zero");
   } else {
     return a / b;
   }
 }
-function addNumbers() {}
+function addNumbers() {
+  if (arguments.length == 0) {
+    throw new Error("You need to pass at least one number");
+  } else if (Array.from(arguments).some((x) => typeof x != "number")) {
+    throw new Error("All parameters must be number");
+  } else {
+    var total = 0;
+    Array.from(arguments).forEach((number) => {
+      total += number;
+    });
+    return total;
+  }
+}
+function analyzeArray(arrayOfNumbers) {
+  if (arguments.length != 1) {
+    throw new Error("You need to pass exactly one parameter");
+  } else if (typeof arrayOfNumbers == "array") {
+    throw new Error("You need to pass array");
+  } else if (arrayOfNumbers.some((x) => typeof x != "number")) {
+    throw new Error("All elements of array must be number");
+  } else {
+    var object = {};
+    object.numberOfElements = arrayOfNumbers.length;
+    object.averageValue =
+      arrayOfNumbers.reduce((a, b) => a + b, 0) / arrayOfNumbers.length || 0;
+    object.maximumValue = Math.max(...arrayOfNumbers);
+    object.minimumValue = Math.min(...arrayOfNumbers);
+    return object;
+  }
+}
 module.exports = {
   substractTwoNumbers,
   multiplyTwoNumbers,
   divideTwoNumbers,
   addNumbers,
+  analyzeArray,
 };
--- main.spec.js
+++ main.spec.js
@@ -1,5 +1,6 @@
 /* EXERCICE 2 */
 const myFunctions = require("./main");
+/* Substract */
 test("Substract 10 and 9", () => {
   expect(myFunctions.substractTwoNumbers(10, 9)).toBe(1);
 });
@@ -8,6 +9,7 @@ test("Substract 10, 9 and null", () => {
     myFunctions.substractTwoNumbers(10, 9, null);
   }).toThrow(Error);
 });
+/* Multiply */
 test("Multiply 2 and 22", () => {
   expect(myFunctions.multiplyTwoNumbers(2, 22)).toBe(44);
 });
@@ -16,6 +18,7 @@ test("Multiply only null", () => {
     myFunctions.multiplyTwoNumbers(null);
   }).toThrow(Error);
 });
+/* Divide */
 test("Divide 10 and 5", () => {
   expect(myFunctions.divideTwoNumbers(10, 5)).toBe(2);
 });
@@ -24,6 +27,45 @@ test("Divide 10 and 0", () => {
     myFunctions.divideTwoNumbers(10, 0);
   }).toThrow(Error);
 });
+/* Add numbers */
 test("Add 1, 4, 10 and 22", () => {
-  expect(myFunctions.addNumbers([1, 4, 10, 22])).toBe(37);
+  expect(myFunctions.addNumbers(1, 4, 10, 22)).toBe(37);
+});
+test("Add 1, null, 10 and 22", () => {
+  expect(() => {
+    myFunctions.addNumbers(1, null, 10, 22);
+  }).toThrow(Error);
+});
+test("Add with no parameters", () => {
+  expect(() => {
+    myFunctions.addNumbers();
+  }).toThrow(Error);
+});
+
+test("Add with strings 1, 2, 3, 4 and 5", () => {
+  expect(() => {
+    myFunctions.addNumbers("1", "2", "3", "4", "5");
+  }).toThrow(Error);
+});
+
+/* Exercise 4 */
+
+const array1 = myFunctions.analyzeArray([1, 2, 3, 4, 5]);
+test("Analyze array of 1, 2, 3, 4, 5", () => {
+  expect(array1.numberOfElements).toBe(5);
+  expect(typeof array1).toBe("object");
+  expect(array1.averageValue).toBe(3);
+  expect(array1.minimumValue).toBe(1);
+  expect(array1.maximumValue).toBe(5);
+});
+test("Analyze array expecting errors", () => {
+  expect(() => {
+    arraymyFunctions.analyzeArray([1, null, "zika", undefined]);
+  }).toThrow(Error);
+  expect(() => {
+    arraymyFunctions.analyzeArray([1, null, "zika", undefined], 2, "zika");
+  }).toThrow(Error);
+  expect(() => {
+    arraymyFunctions.analyzeArray();
+  }).toThrow(Error);
 });
